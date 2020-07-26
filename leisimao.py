5f2aff0059d20ae096add60c81c54b3163ffffab
xiaobaiseven <1146112953@qq.com>
2020-07-26 13:32:39 +0800

    0

---
+++ leisimao.py
@@ -0,0 +1,9 @@
+import requests
+url = 'https://www.cenzqxx.com/qingchun'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.9 Safari/537.36',
+    'Referer': 'https://www.cenzqxx.com'
+}
+response = requests.get(url, headers=headers).content.decode('utf-8')
+print(response)
