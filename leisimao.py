7e28f1efe64164e3b53f62581c98502e8b6c3f9a
xiaobaiseven <1146112953@qq.com>
2020-07-27 00:41:26 +0800

    1

--- leisimao.py
+++ leisimao.py
@@ -1,5 +1,6 @@
 import requests
 url = 'https://www.cenzqxx.com/qingchun'
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                   'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.9 Safari/537.36',
